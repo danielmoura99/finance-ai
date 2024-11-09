c0211771af4b97ef0b56553ff5a45a7770ba3b7e
dsmouraa <dsmouraa@icloud.com>
2024-11-09 11:22:15 -0300

    feat:ensure pagers are correctly scrolled by the user

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,6 +5,7 @@ import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionColumns } from "./_columns";
 import { auth } from "@clerk/nextjs/server";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -20,16 +21,18 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         {/*titulo do botão*/}
         <div className="flex w-full items-center justify-between p-6">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable
-          columns={transactionColumns}
-          data={JSON.parse(JSON.stringify(transactions))}
-        />
+        <ScrollArea>
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   );
